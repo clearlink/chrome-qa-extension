e18151ce7d9e4b186a7cdfac5f82cfaad4daaa6b
Brandon Hatch <brandon.hatch@clearlink.com>
2023-12-19 16:19:04 -0700

    popup.html, cache results, lang detect

--- js/background.js
+++ js/background.js
@@ -4,15 +4,73 @@
  - Sends data back to the popup script.
  */
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "fetchPriorityPages" && request.url) {''
-    // Perform the API call
-    console.log('request.url:', request.url)
-    fetch(`${request.url}/wp-json/coolwhip/v2/priority-pages`)
-        .then(response => response.json())
-        .then(data => sendResponse({data: data}))
-        .catch(error => sendResponse({error: error.toString()}));
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === "fetchPriorityPages" && request.url) {''
+//     // Perform the API call
+//     console.log('request.url:', request.url)
+//     fetch(`${request.url}/wp-json/coolwhip/v2/priority-pages`)
+//         .then(response => response.json())
+//         .then(data => sendResponse({data: data}))
+//         .catch(error => sendResponse({error: error.toString()}));
 
-    return true; // Will respond asynchronously.
+//     return true; // Will respond asynchronously.
+//   }
+// });
+
+// let cache = {};
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   const baseUrl = request.url;
+//   console.log('request.url:', request.url)
+
+//     if (request.action === "fetchData") {
+//         if (cache[baseUrl]) {
+//             sendResponse({data: cache[baseUrl]}); // Send cached data if available
+//         } else {
+//             // Perform the API call
+//             fetch(baseUrl + '/wp-json/coolwhip/v2/priority-pages')
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     cache[baseUrl] = data; // Cache the data
+//                     sendResponse({data: data});
+//                 })
+//                 .catch(error => sendResponse({error: error.toString()}));
+//         }
+//         return true; // Indicates asynchronous response
+//     }
+// });
+
+let cache = {};
+
+const CACHE_LIFETIME = 15 * 60 * 1000; // 15 minutes in milliseconds
+
+function isCacheStale(baseUrl) {
+  if (!cache[baseUrl]) return true; // Cache for URL does not exist
+  const now = new Date().getTime();
+  const cacheAge = now - cache[baseUrl].timestamp;
+  return cacheAge > CACHE_LIFETIME;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  const baseUrl = request.url;
+
+  if (request.action === "fetchData") {
+    if (!isCacheStale(baseUrl) && cache[baseUrl]) {
+      console.log('not stale');
+      sendResponse({data: cache[baseUrl].data}); // Send cached data if not stale
+    } else {
+      // Fetch new data, cache it, and send response
+      fetch(baseUrl + '/wp-json/coolwhip/v2/priority-pages')
+        .then(response => response.json())
+        .then(data => {
+          cache[baseUrl] = {
+            data: data,
+            timestamp: new Date().getTime() // Update timestamp
+          };
+          sendResponse({data: data});
+        })
+        .catch(error => sendResponse({error: error.toString()}));
+    }
+    return true; // Indicates asynchronous response
   }
 });
--- js/popup.js
+++ js/popup.js
@@ -9,34 +9,34 @@ document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     let currentTab = tabs[0];
     if (isEligibleSite(currentTab.url)) {
-      console.log('is eligible site');
-
       const baseUrl = new URL(currentTab.url).origin;
       
       chrome.runtime.sendMessage(
-        {action: "fetchPriorityPages", url: baseUrl},
+        { action: "fetchData", url: baseUrl },
         response => {
           if (response.data) {
             console.log('response data:', response.data);
-              // Update popup DOM with links
+              buildMenu(response.data);
           }
           if (response.error) {
-            console.log('response error:', response.error);
-              // Update popup DOM with error message
+            console.log('response error:', response.error); // this goes to popup console
+            showMessage('An error occurred while fetching data from this site.');
           }
         }
       );
+    } else {
+      showMessage('This extension is not available on this site.');
     }
   });
 });
 
 const liveSites = [
-  'https://www.cabletv.com',
-  'https://www.highspeedinternet.com',
-  'https://www.reviews.org',
-  'https://www.satelliteinternet.com',
-  'https://www.safewise.com',
-  'https://www.business.org',
+  'https://www.cabletv.com/',
+  'https://www.highspeedinternet.com/',
+  'https://www.reviews.org/',
+  'https://www.satelliteinternet.com/',
+  'https://www.safewise.com/',
+  'https://www.business.org/',
   'https://www.move.org/',
 ]
 
@@ -59,4 +59,98 @@ function isLocalEnvironment(url) {
 
 function isEligibleSite(url) {
   return isLiveSite(url) || isFeatureBranch(url) || isLocalEnvironment(url);
+}
+
+function createLinks(key, data, baseUrl) {
+  const container = document.getElementById('links-container');
+  container.innerHTML = ''; // Clear existing links
+
+  const urls = data[key].split('\r\n');
+
+  if (urls.length === 0 || urls[0] === '') {
+    let message = key === 'high_priority_pages' ? 'No Priority Pages found.' : 'No Priority Pages found for this language.';
+    message += '\n\n';
+    message += 'Please wait 15 mins for cache to clear \n';
+    message += 'or contact the CB Eng team if this is an error.';
+    showMessage(message);
+    return;
+  }
+
+  urls.forEach(relativePath => {
+    if (relativePath) {
+      const link = document.createElement('a');
+      link.onclick = function() { 
+        chrome.tabs.update(undefined, {url: baseUrl + relativePath});
+      };
+      link.textContent = relativePath; // Use the absolute URL as link text
+      link.target = '_blank'; // Open in new tab
+      container.appendChild(link);
+      container.appendChild(document.createElement('br')); // For spacing
+    }  
+  });
+}
+
+function getLangFromUrl(url) {
+  const urlPath = new URL(url).pathname;
+  if (urlPath.includes('/au/')) return 'au';
+  if (urlPath.includes('/es/')) return 'es';
+  return 'en'; // Default language
+}
+
+function buildMenu(data, overrideLang = null) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    const currentTab = tabs[0];
+    const baseUrl = new URL(currentTab.url).origin;
+    const defaultLangKey = 'high_priority_pages';
+
+    // Determine the active language: either the override language or from the URL
+    const activeLang = overrideLang || getLangFromUrl(currentTab.url);
+
+    // Construct the key for the data object
+    const langKey = activeLang === 'en' ? defaultLangKey : `${defaultLangKey}_${activeLang}`;
+
+    if (data.hasOwnProperty(langKey)) {
+      createLinks(langKey, data, baseUrl);
+    } else {
+      createLinks(defaultLangKey, data, baseUrl); // Fallback to default if not found
+    }
+
+    createSwitchLinks(data, baseUrl, activeLang);
+  });
+}
+
+function createSwitchLinks(data, baseUrl, activeLang) {
+  const possibleLangs = ['en', 'es', 'au', 'qa'];
+  const footerContainer = document.getElementById('footer-container');
+  footerContainer.innerHTML = ''; // Clear existing links
+
+  possibleLangs.forEach(lang => {
+    if (lang !== activeLang) {
+      const key = lang === 'en' ? 'high_priority_pages' : `high_priority_pages_${lang}`;
+      addSwitchLinkIfExists(`${lang.toUpperCase()} Priority Pages`, key, data, baseUrl, lang);
+    }
+  });
+}
+
+function addSwitchLinkIfExists(text, key, data, baseUrl, lang) {
+  if (data.hasOwnProperty(key)) {
+    const switchLink = document.createElement('a');
+    switchLink.href = '#';
+    switchLink.textContent = text;
+    switchLink.onclick = function(e) {
+      e.preventDefault();
+      buildMenu(data, lang); // Pass the selected language as an override
+    };
+    document.getElementById('footer-container').appendChild(switchLink);
+    document.getElementById('footer-container').appendChild(document.createElement('br'));
+  }
+}
+
+function showMessage(message) {
+  message = message.replace(/\n/g, '<br>'); // Replace newlines with <br>
+  const container = document.getElementById('links-container');
+  container.innerHTML = ''; // Clear existing links
+  const messageEl = document.createElement('p');
+  messageEl.innerHTML = message;
+  container.appendChild(messageEl);
 }
